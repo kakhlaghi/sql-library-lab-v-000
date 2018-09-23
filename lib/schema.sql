dd175d71188611f5e8ceb257274b647df7d75ad0
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-23 20:00:55 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
-  author_id,
-  subgenre_id);
+  author_id INT,
+  subgenre_id INT);
 
 CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
@@ -16,15 +16,15 @@ CREATE TABLE books(
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
-  series_id);
+  series_id INT);
 
 CREATE TABLE characters(
   id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
   species TEXT,
-  author_id,
-  series_id);
+  author_id INT,
+  series_id INT);
 
   CREATE TABLE character_books(
     id INTEGER PRIMARY KEY,
